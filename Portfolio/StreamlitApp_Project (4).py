d18784c2093988d57fc71a517f0f9131b394a45f
aemanoog <a.e.manoog@tcu.edu>
2026-04-28 13:48:35 -0500

    Update StreamlitApp_Project (4).py

--- Portfolio/StreamlitApp_Project (4).py
+++ Portfolio/StreamlitApp_Project (4).py
@@ -71,8 +71,8 @@ MODEL_INFO = {
     "endpoint"  : aws_endpoint,
     "explainer" : "explainer_sentiment.shap",
     "pipeline"  : "finalized_fraud_model.tar.gz",
-    "keys"      : ['TransactionAmt','card6_freq_enc','card3','C12'],
-    "inputs"    : [{"name": k, "type": "number", "min": -1.0, "max": 1.0, "default": 0.0, "step": 0.01} for k in ['TransactionAmt','card6_freq_enc','card3','C12']]
+    "keys"      : ['TransactionAmt','card1','card2','C1'],
+    "inputs"    : [{"name": k, "type": "number", "min": -1.0, "max": 1.0, "default": 0.0, "step": 0.01} for k in ['TransactionAmt','card1','card2','C1']]
 }
 
 
