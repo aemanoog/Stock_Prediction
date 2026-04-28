f50de11158003864797fce0c74ba3208b1f38503
aemanoog <a.e.manoog@tcu.edu>
2026-04-28 12:50:50 -0500

    Update and rename feature_utils.py to Project_feature_utils.py

--- Portfolio/Project_feature_utils.py
+++ Portfolio/Project_feature_utils.py
@@ -7,10 +7,12 @@ import requests
 #from datetime import datetime, timedelta
 import os
 import sys
-import json #
 
-from src.Custom_Classes import FeatureEngineer
+import os
+import sys
 
+
+# ... continue with your script ...
 
 def extract_features():
 
@@ -86,65 +88,3 @@ def get_bitcoin_historical_prices(days = 60):
     df = df[['Date', 'Close Price (USD)']].set_index('Date')
     return df
 
-def convert_input_pca_regression(request_body, request_content_type):
-    print(f"Receiving data of type: {request_content_type}")
-    
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    project_root = os.path.abspath(os.path.join(current_dir, '..'))
-    file_path = os.path.join(project_root, 'Portfolio/SP500Data.csv')
-
-    dataset = pd.read_csv(file_path,index_col=0)
-
-    target = 'AMZN'
-
-    option = 2
-
-    if option == 2:
-
-        X = FeatureEngineer(windows=[5,10,15,20,30]).transform(dataset[[target]])
-    
-        techIndicator_1 = 'MOM_15'
-        MOM_15 = json.loads(request_body)[techIndicator_1]
-        techIndicator_2 = 'ROC_10'
-        ROC_10 = json.loads(request_body)[techIndicator_2]
-
-        # Calculate the distance
-        distances = np.sqrt(
-            (X[techIndicator_1] - MOM_15)**2 + 
-            (X[techIndicator_2] - ROC_10)**2
-        )
-        
-        closest_index = distances.idxmin()
-        closest_row = X.loc[[closest_index]]
-    
-        closest_row[techIndicator_1] = MOM_15
-        closest_row[techIndicator_2] = ROC_10
-    
-        return closest_row
-    else:
-
-        return_period = 5
-
-        SP500_1 = 'IBM_CR_Cum'
-        IBM_CR_Cum = json.loads(request_body)[SP500_1]
-        SP500_2 = 'NVDA_CR_Cum'
-        NVDA_CR_Cum = json.loads(request_body)[SP500_2]
-
-        X = np.log(dataset.drop([target],axis=1)).diff(return_period)
-        X = np.exp(X).cumsum()
-        X.columns = [name + "_CR_Cum" for name in X.columns]
-        
-        # Calculate the distance
-        distances = np.sqrt(
-            (X[SP500_1] - IBM_CR_Cum)**2 + 
-            (X[SP500_2] - NVDA_CR_Cum)**2
-        )
-        
-        closest_index = distances.idxmin()
-        closest_row = X.loc[[closest_index]]
-    
-        closest_row[SP500_1] = IBM_CR_Cum
-        closest_row[SP500_2] = NVDA_CR_Cum
-    
-        return closest_row
-    
